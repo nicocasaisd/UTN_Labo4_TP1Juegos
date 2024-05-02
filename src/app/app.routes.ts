aac160b05c4d94bc16d8b5c3a61266d56dad340f
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-05-02 18:29:53 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./components/home/home.component').then(
+            (m) => m.HomeComponent)
+    },
+    // {
+    //     path: 'login',
+    //     loadComponent: () => import('./components/auth/login/login.component').then(
+    //         (m) => m.LoginComponent)
+    // },
+    {
+        path: 'about',
+        loadComponent: () => import('./components/about/about.component').then(
+            (m) => m.AboutComponent)
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./components/auth/auth.routes').then(
+            (m) => m.routes)
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./components/error/error.component').then(
+            (m) => m.ErrorComponent)
+    },
+    
+];
---
+++ src/app/components/auth/auth.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+
+export const routes: Routes = [
+    // Componentes
+    {
+        path: 'login',
+        loadComponent: () => import('./login/login.component').then(
+            (m) => m.LoginComponent)
+    },
+]
