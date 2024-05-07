c2402b6b55e2f7974c64ab19db403021c376c9d4
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-05-07 15:41:40 -0300

    creo clase user

---
+++ src/app/classes/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    
+    constructor(uid : string, name:string, email:string){}
+
+    private uid: string | undefined;
+    private name: string | undefined;
+    private email: string | undefined;
+}
